259189a48243ed78bb2a33d7e4880731d840051f
Fickel <maakeirwin@gmail.com>
2025-12-05 22:13:38 +0100

    begin aan dag2

---
+++ dag2/ster1/rust/target/debug/deps/rust-c1471767486dca6a.d
@@ -0,0 +1,5 @@
+C:\Users\E. Li\Documents\persoonlijk\code_iets\dag2\ster1\rust\target\debug\deps\rust-c1471767486dca6a.d: src\main.rs
+
+C:\Users\E. Li\Documents\persoonlijk\code_iets\dag2\ster1\rust\target\debug\deps\librust-c1471767486dca6a.rmeta: src\main.rs
+
+src\main.rs:
